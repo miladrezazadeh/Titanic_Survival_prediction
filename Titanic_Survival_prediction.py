1d80ae25f61a25ee3a442fdcf749144f6019f3d8
Milad Rezazadeh <miladrezazadeh72@gmail.com>
2021-04-15 21:26:11 -0400

    Import required directories

---
+++ Titanic_Survival_prediction.py
@@ -0,0 +1,12 @@
+## Import required libraries
+import numpy as np
+import pandas as pd
+import pickle
+import re
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+
+## Import the data
