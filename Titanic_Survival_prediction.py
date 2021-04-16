795747e9c7aa41522508b3dfca0e0ad4d1809076
Milad Rezazadeh <miladrezazadeh72@gmail.com>
2021-04-15 22:31:39 -0400

    make a copy of the original data

--- Titanic_Survival_prediction.py
+++ Titanic_Survival_prediction.py
@@ -15,6 +15,12 @@ test_data = '/Users/milad/OneDrive - Dalhousie University/Titanic_Github/Titanic
 df_train = pd.read_csv(train_data)
 df_test = pd.read_csv(test_data)
 
-#make a copy of the original data
-df_train_or = =df_train.copy()
+##make a copy of the original data
+df_train_or = df_train.copy()
 df_test_or = df_test.copy()
+
+## Data analysis
+#print(df_train.info())
+## 891 entries, 12 columns
+
+print(df_test.info())
