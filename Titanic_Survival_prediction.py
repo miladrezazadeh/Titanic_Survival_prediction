6c7146d710609cca5ac9902879252bf8fa32e72f
Milad Rezazadeh <miladrezazadeh72@gmail.com>
2021-04-15 21:53:31 -0400

    Importing training and testing datasets

--- Titanic_Survival_prediction.py
+++ Titanic_Survival_prediction.py
@@ -10,3 +10,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 ## Import the data
+train_data = '/Users/milad/OneDrive - Dalhousie University/Titanic_Github/Titanic_Survival_prediction/train.csv'
+test_data = '/Users/milad/OneDrive - Dalhousie University/Titanic_Github/Titanic_Survival_prediction/test.csv'
+df_train = pd.read_csv(train_data)
+df_test = pd.read_csv(test_data)
